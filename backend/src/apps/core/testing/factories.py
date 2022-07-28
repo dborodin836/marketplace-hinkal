799943df3c8eb6618e568482d377f3491be840bb
dborodin836 <dborodin836@gmail.com>
2022-07-29 01:54:22 +0300

    REFACTOR: Backend: refactored VendorFactory

--- backend/src/apps/core/testing/factories.py
+++ backend/src/apps/core/testing/factories.py
@@ -7,13 +7,6 @@ import factory
 from src.apps.user.models import Vendor
 
 
-def get_name(x):
-    return (
-        "".join([random.choice(string.ascii_letters + "123456789_-") for _ in range(30)])
-        + str(datetime.now())[:10]
-    )
-
-
 class VendorFactory(factory.Factory):
     """
     Creates a new ``Vendor`` object.
@@ -22,7 +15,12 @@ class VendorFactory(factory.Factory):
     Password will be ``testpass`` by default.
     """
 
-    username = factory.LazyAttribute(get_name)
+    @factory.lazy_attribute
+    def username(self):
+        return (
+                "".join([random.choice(string.ascii_letters + "123456789_-") for _ in range(30)])
+                + str(datetime.now())[:10]
+        )
     email = f"{username}@example.com"
     password = factory.PostGenerationMethodCall("set_password", "testpass")
     is_active = True
